58d0919b207979307121132d16a8a85268e8226b
beulah444 <beulah444@gmail.com>
2017-08-28 17:33:08 -0700

    Add project files.

---
+++ nthLargestNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nthLargestNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = new[] { 5, 7, 5, 3, 6, 7, 9 };
+            int n = 4;
+            var result = numbers.GroupBy(x => x)
+                    .OrderByDescending(group => group.Key)
+                    .Take(n)
+                    .Select(group => group.Key)
+                    .ToArray();
+            Console.WriteLine("num={0}", result[3]);
+            Console.ReadKey();
+
+        }
+    }
+}
